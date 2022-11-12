7b876fd818cc97e62c7f692c4a418380dfa57127
KayHancock <106179836+KayHancock@users.noreply.github.com>
2022-11-12 01:39:13 -0500

    linked files to index

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
+
+const Manager = require("./roles/manager");
+const Engineer = require("./roles/engineer");
+const Intern = require("./roles/intern");
